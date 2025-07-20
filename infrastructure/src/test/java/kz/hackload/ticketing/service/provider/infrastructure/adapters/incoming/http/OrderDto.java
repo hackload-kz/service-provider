380b5306b8c4aa1ec8c2357d122e7d0ae0de90a5
Teymur Shaykemelov <shaykemelov@gmail.com>
2025-07-20 12:57:09 +0500

    Added missing file

---
+++ infrastructure/src/test/java/kz/hackload/ticketing/service/provider/infrastructure/adapters/incoming/http/OrderDto.java
@@ -0,0 +1,37 @@
+package kz.hackload.ticketing.service.provider.infrastructure.adapters.incoming.http;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+import kz.hackload.ticketing.service.provider.domain.orders.OrderId;
+import kz.hackload.ticketing.service.provider.domain.orders.OrderStatus;
+
+@JsonDeserialize(using = OrderDto.OrderDtoDeserializer.class)
+public record OrderDto(OrderId id, OrderStatus status, Instant startedAt, Instant updatedAt, long placesCount)
+{
+    public static final class OrderDtoDeserializer extends JsonDeserializer<OrderDto>
+    {
+        @Override
+        public OrderDto deserialize(final JsonParser p, final DeserializationContext ctxt) throws IOException, JacksonException
+        {
+            final TreeNode treeNode = p.getCodec().readTree(p);
+            final OrderId orderId = new OrderId(UUID.fromString(((JsonNode) treeNode.get("id")).asText()));
+            final OrderStatus orderStatus = OrderStatus.valueOf(((JsonNode) treeNode.get("status")).asText());
+            final Instant startedAt = Instant.from(DateTimeFormatter.ISO_INSTANT.parse(((JsonNode) treeNode.get("started_at")).asText()));
+            final Instant updatedAt = Instant.from(DateTimeFormatter.ISO_INSTANT.parse(((JsonNode) treeNode.get("updated_at")).asText()));
+            final long placesCount = ((JsonNode) treeNode.get("places_count")).asLong();
+
+            return new OrderDto(orderId, orderStatus, startedAt, updatedAt, placesCount);
+        }
+    }
+}
